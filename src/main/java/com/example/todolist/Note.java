7e7d656f90c773be7f6c469265b11ecfa6a2b963
SofiiaZasymenko <87094575+SofiiaZasymenko@users.noreply.github.com>
2023-08-07 00:43:51 +0300

    Module 14 homework

--- src/main/java/com/example/todolist/Note.java
+++ src/main/java/com/example/todolist/Note.java
@@ -1,8 +1,19 @@
 package com.example.todolist;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String title;
+    @Column
     private String content;
 
     public Note() {
